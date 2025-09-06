017ec3660dc0b942f0d3276e0e56ebcf371e82b1
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-09-06 13:32:06 -0300

    hice la configuracion del servidor

---
+++ server/config.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import "../db/config.js";
+
+export default class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+    this.middlewares();
+  }
+  middlewares() {
+    this.app.use(cors());
+    this.app.use(morgan("dev"));
+    this.app.use(express.json());
+    const __dirname = dirname(fileURLToPath(import.meta.url));
+    this.app.use(express.static(`${__dirname}/../public`));
+  }
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log(
+        `El servidor se esta ejecutando en: http://localhost:${this.port}`
+      );
+    });
+  }
+}

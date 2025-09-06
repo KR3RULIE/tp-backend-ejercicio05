c762490bf84afb178df7d85dc1ff687dc4cfae18
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-09-06 15:16:12 -0300

    cree los endpoints de usuario y recetas

--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -1,5 +1,20 @@
 import { Router } from "express";
+import {
+  crearReceta,
+  leerRecetas,
+  leerRecetaID,
+  borrarReceta,
+  editarReceta,
+} from "../controllers/recetas.controllers.js";
+import validarReceta from "../middleware/validarRecetas";
 
 const router = Router();
+
+router.use("/").get(leerRecetas).post(validarReceta, crearReceta);
+router
+  .use("/:id")
+  .get(leerRecetaID)
+  .delete(borrarReceta)
+  .put(validarReceta, editarReceta);
 
 export default router;
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express";
+import {
+  crearUsuario,
+  leerUsuarios,
+} from "../controllers/usuarios.controllers";
+import validacionUsuario from "../middleware/validarUsuario";
 
 const router = Router();
+
+router.get("/").get(leerUsuarios).post(validacionUsuario, crearUsuario);
 
 export default router;

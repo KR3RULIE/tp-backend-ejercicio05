69cd896a826cf37a20090005cf771245cc08e2eb
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-09-06 15:07:51 -0300

    cree la rutas de usuarios, modelo, validacion y endpoints

---
+++ src/controllers/usuarios.controllers.js
@@ -0,0 +1,35 @@
+import Usuario from "../models/usuario.js";
+import bcrypt from "bcrypt";
+
+export const leerUsuarios = async (req, res) => {
+  try {
+    const listaUsuarios = await Usuario.find();
+    res.status(200).json(listaUsuarios);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al leer los usuarios" });
+  }
+};
+
+export const crearUsuario = async (req, res) => {
+  try {
+    const { nombreUsuario, email, password } = req.body;
+    //hashear el password
+    const saltos = bcrypt.genSaltSync(10);
+    const passwordHash = bcrypt.hashSync(password, saltos);
+
+    const nuevoUsuario = new Usuario({
+      nombreUsuario,
+      email,
+      password: passwordHash,
+    });
+    await nuevoUsuario.save();
+    //4- enviar respuesta
+    res.status(201).json({
+      mensaje: `El usuario: ${nuevoUsuario.nombreUsuario} fue registrado exitosamente`,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al crear el usuario" });
+  }
+};

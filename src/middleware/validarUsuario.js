21c35803e09b7a23f868d76ea0cb928bcb7b7980
KR3RULIE <mjt.kr3rulie@gmail.com>
2025-09-07 15:57:32 -0300

    cambiar el maximo de la pw de 16 a 100

--- src/middleware/validarUsuario.js
+++ src/middleware/validarUsuario.js
@@ -17,10 +17,10 @@ const validacionUsuario = [
   body("pw")
     .notEmpty()
     .withMessage("La contraseña es un dato obligatorio")
-    .isLength({ min: 8, max: 16 })
+    .isLength({ min: 8, max: 100 })
     .withMessage("La contraseña debe tener entre 8 y 16 caracteres")
     .matches(
-      /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,16}$/
+      /^(?=.*\d)(?=.*[\u0021-\u002b\u003c-\u0040])(?=.*[A-Z])(?=.*[a-z])\S{8,100}$/
     )
     .withMessage(
       "La contraseña debe contener al menos una letra mayúscula, una letra minúscula, un número y un carácter especial"
